af3aee3a4c9776f40b22cf49e62459036a66d14b
Brendan O'Bra <brendan@obrafamily.org>
2019-02-22 15:03:37 -0800

    Update maven-build.sh

--- tasks/maven-build.sh
+++ tasks/maven-build.sh
@@ -22,11 +22,11 @@ mvn versions:set -DnewVersion=${VERSION}
 echo "Building artifact ..."
 mvn package -DskipTest ${MAVEN_ARGS}
 echo "Copying artifacts to ../build "
-mkdir -p ../build/docker/app/lib
-mkdir -p ../build/docker/app/META-INF
+mkdir -p ../build/app/lib
+mkdir -p ../build/app/META-INF
 
-cp -r target/dependency/BOOT-INF/lib ../build/docker/app/lib
-cp -r target/dependency/META-INF ../build/docker/app/META-INF
-cp -r target/dependency/BOOT-INF/classes ../build/docker/app
-cp Dockerfile.concourse ../build/docker/Dockerfile
+cp -r target/dependency/BOOT-INF/lib ../build/app/lib
+cp -r target/dependency/META-INF ../build/app/META-INF
+cp -r target/dependency/BOOT-INF/classes ../build/app
+cp Dockerfile.concourse ../build/Dockerfile
 
