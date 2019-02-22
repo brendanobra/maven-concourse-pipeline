13aaf26b40d39ad06811da797e445e248cb918f2
Brendan O'Bra <brendan@obrafamily.org>
2019-02-22 14:35:13 -0800

    Update maven-build.sh

--- tasks/maven-build.sh
+++ tasks/maven-build.sh
@@ -14,7 +14,6 @@ source ./pipeline/tasks/common.sh
 VERSION=$(build_version "./version" "number" "./source-code" $BRANCH)
 echo "Version to build: ${VERSION}"
 
-
 cd source-code
 
 echo "Setting maven with version to build"
@@ -22,5 +21,12 @@ mvn versions:set -DnewVersion=${VERSION}
 
 echo "Building artifact ..."
 mvn package -DskipTest ${MAVEN_ARGS}
-echo "pwd="
-echo `pwd`
+echo "Copying artifacts to ../build "
+mkdir -p ../build/docker/app/lib
+mkdir -p ../build/docker/app/META-INF
+
+cp target/dependency/BOOT-INF/lib ../build/docker/app/lib
+cp target/dependency/META-INF ../build/docker/app/META-INF
+cp target/dependency/BOOT-INF/classes ../build/docker/app
+cp Dockerfile ../build/docker
+
